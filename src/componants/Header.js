48a9dde5d8948d16e069bfa3af0e992a15ad672c
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-18 21:48:58 -0600

    Add header componant file

---
+++ src/componants/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = props => {
+      return (
+        <div className='header'>
+        {/* <img 
+            src={require('./images/Logo.')} 
+            alt='logo'
+        /> */}
+        <h1>CodeKettle</h1>
+        </div>
+    )
+}
+export default Header
