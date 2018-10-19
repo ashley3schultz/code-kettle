f436b29ab717baa9d9f7c28643b158d9dcb2b802
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-18 21:49:58 -0600

    Add about componant file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './componants/Header';
+import About from './componants/About';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <Header/>
+          <About/>
         </header>
       </div>
     );
---
+++ src/componants/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = props => {
+      return (
+        <div className='projects'>
+            <h3>About</h3>
+            {/* <img 
+                src={require('./images/Logo.')} 
+                alt='logo'
+            /> */}
+        </div>
+    )
+}
+export default About
