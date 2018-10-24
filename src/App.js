5959f186cd5f250d55bc6eaa75b0746d6e3eafb3
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-23 23:19:45 -0600

    Update App.js file to add flatten array features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import NavBar from "./components/NavBar"
-import Header from './componants/Header';
-import About from './componants/About';
-import Blog from './componants/Blog';
-import Projects from './componants/Projects';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    input: [],
+    array: []
+  }
+
+  handleInput = (event) => {
+    var RegExpression = /[0-9,[\]]/;
+    if(RegExpression.test(event.target.value)) {
+      this.setState({input: event.target.value})
+    }
+    console.log(this.state.input)
+  }
+
+  flattenArray = (array) => {
+
+    var newArray = []
+
+    var deepFlatten = (b) => {
+      if(b.length > 1) {
+        b.forEach((c) => { 
+          deepFlatten(c) 
+        })
+      } else {
+        newArray.push(b)
+      }
+    }
+
+    array.forEach((a) => { 
+      deepFlatten(a) 
+    })
+
+    return newArray
+    
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    var nestedArray = JSON.parse(this.state.input)
+    this.setState({array: this.flattenArray(nestedArray)})
+    console.log("Before: ", this.state.input)
+  }
+
   render() {
+    var array = this.state.array.length ? "[" + this.state.array.toString().replace(/,/g, ", ") + "]" : null
     return (
-        <BrowserRouter>
-          <div className="App">
-            <Header/>
-            <NavBar/>
-            <Switch>
-              <Route path="/" component={About} exact/>
-              <Route path="/projects" component={Projects}/>
-              <Route path="/blog" component={Blog}/>
-            </Switch>
-          </div>
-        </BrowserRouter>
+        <div className="App">
+          <form onSubmit={this.handleSubmit}>
+            <input 
+            onChange={this.handleInput}
+            type="text" 
+            value={this.state.input} 
+            paceholder="Input Nested Array"
+            />
+            <input type="submit" value="Flatten"/>
+          </form>
+          <h1>{array}</h1>
+          {this.state.array.length ? console.log("After: ", this.state.array) : null}
+        </div>
     );
   }
 }
