3dece0386dd8ef5051a51db24937410c7de52e98
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-28 15:28:50 -0600

    Fix delted files

---
+++ src/componants/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from "react-router-dom"
+
+const Navbar = props => {
+      return (
+        <div className="navbar">
+            <NavLink to='/'>About</NavLink>
+            <NavLink to='/projects'>Projects</NavLink>
+            <NavLink to='/blog'>Blog</NavLink>
+        </div>
+    )
+}
+
+export default Navbar
