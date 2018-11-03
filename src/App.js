b76929a7998714cc6381a61c29c6c9c114053719
ashley3schultz <ashleyeschultz@gmail.com>
2018-11-03 09:40:08 -0600

    Fix app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import NavBar from "./components/NavBar"
-import Header from './componants/Header';
-import About from './componants/About';
-import Blog from './componants/Blog';
-import Projects from './componants/Projects';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Blog from './components/Blog';
+import Projects from './components/Projects';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +14,7 @@ class App extends Component {
         <BrowserRouter>
           <div className="App">
             <Header/>
-            <NavBar/>
+            <Navbar/>
             <Switch>
               <Route path="/" component={About} exact/>
               <Route path="/projects" component={Projects}/>
