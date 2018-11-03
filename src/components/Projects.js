b11141cfac01bbe25d90f7a5318fbf7ff32ef066
ashley3schultz <ashleyeschultz@gmail.com>
2018-11-03 10:45:37 -0600

    Update projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import {projects} from './Data'
 
 const Projects = props => {
       return (
         <div className='projects'>
-            <h3>List of Projects</h3>
-            {/* <img 
-                src={require('./images/Logo.')} 
-                alt='logo'
-            /> */}
+            {projects.map(proj => {
+                return (
+                    <div>
+                        <h2>{proj.title}</h2>
+                        <p>{proj.description}</p>
+                        <a href={proj.url}>View Project</a>
+                    </div>
+                )
+            })}
         </div>
     )
 }
