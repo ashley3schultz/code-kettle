a8c74cd23975f4c06c82032ad1f607b05c7b257f
ashley3schultz <ashleyeschultz@gmail.com>
2018-11-15 22:01:12 -0700

    Format data for easier view

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,11 +1,15 @@
 export const projects = [
+
     {title: "Tea-Connoissuer", 
       decsription: "An application for tea connoisseur to add teas to the main collection and post their tasting notes.",
       url: "https://tea-connoisseur.herokuapp.com/"},
+
     {title: "Teafolio", 
       decsription: "Admin controlled application where users can sign up and create tea portfolios.",
       url: "https://teafolio.herokuapp.com/"},
+
     {title: "Superiori-Tea", 
       decsription: "Superiori-Tea is a tea harvesting game. Built with react framework. Separate Rails API built for persistence.",
       url: "https://superiori-tea.herokuapp.com/"}
+      
     ]
