32b1ea8077952294de590d7edf1edba307a6462e
ashley3schultz <ashleyeschultz@gmail.com>
2019-01-06 21:22:38 -0700

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="navbar">
-            <NavLink to='/'>About</NavLink>
-            <NavLink to='/projects'>Projects</NavLink>
-            <NavLink to='/blog'>Blog</NavLink>
+            <NavLink to='/'> About </NavLink>
+            <NavLink to='/projects'> Projects </NavLink>
+            <NavLink to='/blog'> Blog </NavLink>
         </div>
     )
 }
