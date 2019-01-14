d94e5c005ce2312e005714cd8e3bb1ba72d81d55
ashley3schultz <ashleyeschultz@gmail.com>
2019-01-13 19:33:35 -0700

    Update projects componant - project class

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,10 +6,11 @@ const Projects = props => {
         <div className='projects'>
             {projects.map(proj => {
                 return (
-                    <div>
+                    <div className="project">
                         <h2>{proj.title}</h2>
-                        <p>{proj.description}</p>
-                        <a href={proj.url}>View Project</a>
+                        <p>{proj.info}</p>
+                        <a className="proj-link" href={proj.url} target="_blank" rel="noopener noreferrer">
+                        View Project</a>
                     </div>
                 )
             })}
