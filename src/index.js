da677145e9e1a3171516c632afff55d6821b79d3
ashley3schultz <ashleyeschultz@gmail.com>
2019-01-19 12:18:43 -0700

    Add redux/store to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {createStore, compose, applyMiddleware} from 'redux';
+import reducer from './reducers/portfolioReducer'
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+    composeEnhancer(applyMiddleware(thunk)),
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+  );
+registerServiceWorker();
