1f6fa00d80fb9bab2c3033911e62681c312e0cb4
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-17 19:59:58 -0700

    Add fetch request for blog by name

--- src/actions/portfolio.js
+++ src/actions/portfolio.js
@@ -1,8 +1,8 @@
-const API_URL = "https://api.github.com/repos/ashley3schultz/ashley3schultz.github.io/contents/_posts/?per_page=100&token=c7ca424320830b2b3729a149beb05feca5b7e56"
-
+const API_URL = "https://api.github.com/repos/ashley3schultz/ashley3schultz.github.io/contents/_posts/"
+const TOKEN = "?per_page=100&token=c7ca424320830b2b3729a149beb05feca5b7e56"
 export const fetchBlogs = () => {
     return dispatch => {
-        return fetch(API_URL)
+        return fetch(API_URL + TOKEN)
         .then(response => response.json())
         .then(blogs => dispatch(updateBlogs(blogs)))
         .catch(error => console.log(error));
@@ -10,5 +10,19 @@ export const fetchBlogs = () => {
 }
 
 export const updateBlogs = (blogs) => {
-    return { type: "UPDATE_BLOGS", blogs: blogs }
+    return { type: "UPDATE_BLOGS", blogs: blogs.reverse() }
+}
+
+export const fetchBlog = (name) => {
+    return dispatch => {
+        return fetch(API_URL + name)
+        .then(response => response.json())
+        .then(blog => dispatch(updateBlog(blog)))
+        .catch(error => console.log(error));
+    }
+}
+
+
+export const updateBlog = (blog) => {
+    return { type: "UPDATE_BLOG", blog: blog}
 }
