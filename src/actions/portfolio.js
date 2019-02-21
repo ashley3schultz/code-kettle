c4ddebf227dc53083cb93f4efa9dc2367e8df773
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-20 22:04:05 -0700

    Update actions get env variables

--- src/actions/portfolio.js
+++ src/actions/portfolio.js
@@ -1,6 +1,8 @@
-const API_URL = "https://api.github.com/repos/ashley3schultz/ashley3schultz.github.io/contents/_posts/"
-const TOKEN = "?per_page=100&token=c7ca424320830b2b3729a149beb05feca5b7e56"
+const API_URL = process.env.REACT_APP_BLOG_API
+const TOKEN = process.env.REACT_APP_TOKEN
+
 export const fetchBlogs = () => {
+    console.log(TOKEN)
     return dispatch => {
         return fetch(API_URL + TOKEN)
         .then(response => response.json())
@@ -10,19 +12,21 @@ export const fetchBlogs = () => {
 }
 
 export const updateBlogs = (blogs) => {
-    return { type: "UPDATE_BLOGS", blogs: blogs.reverse() }
+    console.log("updateBlogS")
+    return { type: "UPDATE_BLOGS", blogs: blogs }
 }
 
 export const fetchBlog = (name) => {
+    console.log("fetchBlog")
     return dispatch => {
-        return fetch(API_URL + name)
+        return fetch(API_URL + name + TOKEN)
         .then(response => response.json())
         .then(blog => dispatch(updateBlog(blog)))
         .catch(error => console.log(error));
     }
 }
 
-
 export const updateBlog = (blog) => {
+    console.log("updateBlog")
     return { type: "UPDATE_BLOG", blog: blog}
 }
