499d384355e103d547ef0da5274c5fd48bae03d4
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-25 22:25:04 -0700

    Add link to blog list to navigate to blog show

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import Blog from './components/Blog';
-// var showdown  = require('showdown'),
-//     converter = new showdown.Converter()
-// var mkdn = converter.makeHtml(props.blog)
+import { NavLink } from "react-router-dom"
 
 const BlogList = props => {
       return (
         <div className='bloglist'>
             {props.blogs.map(blog => 
-                <div key={blog.name} className="blog">
-                    <h2 className="blog-title">{blog.name.substr(11).replace(/(.markdown)|_/gi," ").replace(/^(.)|\s(.)/g, ($1) => $1.toUpperCase())}</h2>
+                <div key={blog.name} className="blog-preview">
+                    <h2 className="blog-title">{blog.name.substr(11).replace(/(.markdown)|_/gi," ").replace(/^(.)|\s(.)/gi, ($1) => $1.toUpperCase())}</h2>
                     <h3 className="blog-date">{blog.name.substr(0,10)}</h3>
-                    <div className="blog-content">{props.blog.content}</div>
-                    <a className="blog-url" href={blog.url}>Read This Blog</a>
+                    <NavLink 
+                        onClick={props.handleLinkClick} 
+                        to={'/blog/' + blog.name.replace(/(.markdown)/gi,"")}>
+                        Read Blog
+                    </NavLink>
                 </div>
             )}
         </div>
