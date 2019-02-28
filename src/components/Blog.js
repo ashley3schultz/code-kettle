315218f95e1ee0b4b0ead6f0f1435a849087413d
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-27 22:05:45 -0700

    Add title and date to blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Blog = props => {
     var showdown  = require('showdown'),
         converter = new showdown.Converter(),
-        mytext = props.blog.output,
+        mytext = props.blog.content,
         myhtml = converter.makeHtml(mytext)
-        console.log(myhtml)
     return (
         <div className="blog-content">
+            <h2 className="blog-title">{props.blog.title}</h2>
+            <p className="blog-date">{props.blog.date}</p>
             <div dangerouslySetInnerHTML={{__html: myhtml}} />
         </div>
 )}
