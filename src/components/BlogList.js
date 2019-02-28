b02ca8258dc36bd02be65d706d1c4d8fede9fe3a
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-27 22:05:22 -0700

    Update formatting for blog list

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,14 +5,15 @@ const BlogList = props => {
       return (
         <div className='bloglist'>
             {props.blogs.map(blog => 
-                <div key={blog.name} className="blog-preview">
-                    <h2 className="blog-title">{blog.name.substr(11).replace(/(.markdown)|_/gi," ").replace(/^(.)|\s(.)/gi, ($1) => $1.toUpperCase())}</h2>
-                    <h3 className="blog-date">{blog.name.substr(0,10)}</h3>
-                    <NavLink 
-                        onClick={props.handleLinkClick} 
-                        to={'/blog/' + blog.name.replace(/(.markdown)/gi,"")}>
-                        Read Blog
-                    </NavLink>
+                <div key={blog.title} className="blog-preview">
+                    <h2 className="blog-title">
+                        <NavLink 
+                            onClick={props.handleLinkClick} 
+                            to={blog.path}>
+                            {blog.title}
+                        </NavLink>
+                    </h2>
+                    <p className="blog-date">{blog.date}</p>
                 </div>
             )}
         </div>
