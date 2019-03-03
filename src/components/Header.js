41da18b53198f22b0c014ca896950aab79feec71
ashley3schultz <ashleyeschultz@gmail.com>
2019-03-02 18:33:20 -0700

    Update header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = props => {
       return (
         <div className='header'>
         <img 
-            src={require('./images/Logo.png')} 
+            src={require('./images/Logo1.png')} 
             alt='logo'
         />
         </div>
