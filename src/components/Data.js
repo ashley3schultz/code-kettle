08fd1115cc5c61102d8cf21d7aac3eaedd97b068
ashley3schultz <ashleyeschultz@gmail.com>
2019-03-02 18:33:04 -0700

    Update data info and order

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,15 +1,18 @@
 export const projects = [
 
-    {title: "Tea-Connoisseur", 
-      info: "An application for tea connoisseur to add teas to the main collection and post their tasting notes.",
-      url: "https://tea-connoisseur.herokuapp.com/"},
+    {title: "Superiori-Tea", 
+    info: "A tea harvesting game. Built with react framework. Separate Rails API built for persistence.",
+    built: "React/Redux",
+    url: "https://superiori-tea.herokuapp.com/"},
 
     {title: "Teafolio", 
       info: "Admin controlled application where users can sign up and create tea portfolios.",
+      built: "Ruby on Rails and Javascript",
       url: "https://teafolio.herokuapp.com/"},
 
-    {title: "Superiori-Tea", 
-      info: "A tea harvesting game. Built with react framework. Separate Rails API built for persistence.",
-      url: "https://superiori-tea.herokuapp.com/"}
+    {title: "Tea-Connoisseur", 
+    info: "An application for tea connoisseur to add teas to the main collection and post their tasting notes.",
+    built: "Ruby using Sinatra",
+    url: "https://tea-connoisseur.herokuapp.com/"}
       
     ]
