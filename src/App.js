a99a99027989653b6ca67ac2e6d858d68ccb59a5
ashley3schultz <ashleyeschultz@gmail.com>
2019-03-15 13:44:03 -0600

    Update projects path to container

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
 import BlogContainer from './containers/BlogContainer';
-import Projects from './components/Projects';
+import ProjectsContainer from './containers/ProjectsContainer';
 import './App.css';
 
 class App extends Component {
@@ -19,7 +19,7 @@ class App extends Component {
             <Switch>
               <Route path="/" component={About} exact/>
               <Route path="/about" component={About}/>
-              <Route path="/projects" component={Projects}/>
+              <Route path="/projects" component={ProjectsContainer}/>
               <Route path="/blog" component={BlogContainer}/>
             </Switch>
           </div>
