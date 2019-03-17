1723bb480bfe43a5a5c96f6aac57bfeceeff3c22
ashley3schultz <ashleyeschultz@gmail.com>
2019-03-16 22:23:41 -0600

    Change attribute in data

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,18 +4,18 @@ export const projects = [
   info: "A tea harvesting game. Built with react framework. Separate Rails API built for persistence.",
   built: "React/Redux",
   url: "https://superiori-tea.herokuapp.com/",
-  animation: "standby"},
+  status: "standby"},
 
   {title: "Teafolio", 
     info: "Admin controlled application where users can sign up and create tea portfolios.",
     built: "Ruby on Rails and Javascript",
     url: "https://teafolio.herokuapp.com/",
-    animation: "standby"},
+    status: "standby"},
 
   {title: "Tea-Connoisseur", 
   info: "An application for tea connoisseur to add teas to the main collection and post their tasting notes.",
   built: "Ruby using Sinatra",
   url: "https://tea-connoisseur.herokuapp.com/",
-  animation: "standby"}
+  status: "standby"}
       
 ]
