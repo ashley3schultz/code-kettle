fbd3f6ca4d89897fc9092f48333f25b5f1ce7952
ashley3schultz <ashleyeschultz@gmail.com>
2019-03-16 22:26:04 -0600

    Rename projects attribute in reducer

--- src/reducers/portfolioReducer.js
+++ src/reducers/portfolioReducer.js
@@ -2,7 +2,9 @@ export default function portfolioReducer(
     state = {
         blogs: [],
         blog: {},
-        projects: []
+        projects: [],
+        project: {},
+        animation: ""
     }, action) {
     switch (action.type) {
 
@@ -12,17 +14,23 @@ export default function portfolioReducer(
         case "UPDATE_BLOG":
             return {...state, blog: action.blog }
 
-        case "UPDATE_ANIMATION":
-            const projects = state.projects.map(proj => {
-                let ani = proj.title === action.title ? action.animation : "standby"
-                proj.animation = ani
-                return proj
+        case "UPDATE_STATUS":
+            const projects = state.projects.map(project => {
+                let status = project.title === action.title ? "featured" : "standby"
+                project.status = status
+                return project
             })
             return {...state, project: projects}
 
         case "UPDATE_PROJECTS":
             return {...state, projects: action.projects }
-            
+
+        case "UPDATE_PROJECT":
+            return {...state, project: action.project }
+        
+        case "UPDATE_ANIMATION":
+            return {...state, animation: action.animation }
+
         default:
             return state;
     } 
