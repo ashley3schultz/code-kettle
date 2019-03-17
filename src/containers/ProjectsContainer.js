d316bd78733e43df33e4a74d1d821c82d0ee98f0
ashley3schultz <ashleyeschultz@gmail.com>
2019-03-16 22:25:24 -0600

    Update delay animation and comonentdidupdate function

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -2,65 +2,53 @@ import React, { Component } from 'react';
 import ProjectsList from '../components/ProjectsList';
 import Project from '../components/Project';
 import { connect } from 'react-redux'
-import { fetchProjects, updateProjects, updateAnimation } from "../actions/portfolio";
+import { updateProjects, updateStatus, updateProject, updateAnimation } from "../actions/portfolio";
 import { projects } from '../components/Data'
 
-class ProjectsContainer extends Component {
+const delay = (ms) => new Promise(res => setTimeout(res, ms));
 
-  state = {
-    project: [],
-    animation: "",
-    delay: 0
-  }
+class ProjectsContainer extends Component {
 
   componentDidMount() {
     this.props.updateProjects(projects)
   }
 
   componentDidUpdate(prevProps) {
-    const pDif = this.state.project !== prevProps.project
-    const pSet = this.state.project.length
-    const dDif = this.state.delay !== prevProps.delay
-    const dSet = this.state.delays
-    if (pDif && dDif && pSet && dSet) {
-      this.handleAnimation()
+    const project = this.props.project
+    const aniDif = this.props.animation !== prevProps.animation
+    const loaded = this.props.animation === 'loaded'
+    if (aniDif && loaded && !!project) {
+      this.delayAnimation()
     }
   }
 
-  handleAnimation = () => {
-    console.log(this.state.delay)
-    if (this.state.delay > 0) {
-      this.setState(prevState => {
-        return { delay: prevState.delay - 1}
-      });
-      setTimeout(this.handleAnimation(),1000)
-    } else {
-      this.setState({animation: 'playing'})
-    }
+  delayAnimation = async () => {
+    await delay(100);
+    this.props.updateAnimation('playing')
   }
 
   handleProjectHover = (event) => {
-    const a = event.target.getAttribute('class')
-    const t = event.target.getAttribute('alt')
-    if (a === "standby"){
-      this.props.updateAnimation(t)
-      const proj = this.props.projects.find(p => p.title === t)
-      this.setState({project: [proj], animation: 'loaded', delay: 5}) 
+    const status = event.target.getAttribute('class')
+    if (status === "standby"){
+      const title = event.target.getAttribute('alt')
+      const project = this.props.projects.find(p => p.title === title)
+      this.props.updateStatus(title)
+      this.props.updateProject(project)
+      this.props.updateAnimation('loaded')
     }
   }
 
   render() {
-    console.log(this.state)
     return (
         <div className="ProjectsContainer">
             <ProjectsList 
               projects={this.props.projects} 
               handleProjectHover={this.handleProjectHover}
             />
-            {this.state.animation ? 
+            {this.props.animation ? 
             <Project
-              project={this.state.project}
-              animation={this.state.animation} 
+              project={this.props.project}
+              animation={this.props.animation} 
             /> : <div></div>}
         </div>
     )
@@ -69,8 +57,11 @@ class ProjectsContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    projects: state.projects
+    projects: state.projects,
+    project: state.project,
+    animation: state.animation,
+    delay: state.delay
   }
 }
 
-export default connect(mapStateToProps, {fetchProjects, updateProjects, updateAnimation})(ProjectsContainer)
+export default connect(mapStateToProps, {updateProjects, updateStatus, updateProject, updateAnimation})(ProjectsContainer)
